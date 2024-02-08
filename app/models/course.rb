72c1e1d65a449e13ca37c99e45443dd1bfaf44bc
Jerry Wang <ziyanwang1105@gmail.com>
2024-02-08 13:33:14 -0800

    finish project 1

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,33 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_students,
+        through: :enrollments,
+        source: :student
+
+    belongs_to :prerequisite,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course,
+        optional: true
+
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_courses,
+        through: :enrollments,
+        source: :course
+
+    has_many :courses,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :Course
 end
