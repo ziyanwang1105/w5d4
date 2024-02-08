d3f2e8f7df676681c91c8a26d606aeca255c12ef
Jerry Wang <ziyanwang1105@gmail.com>
2024-02-08 15:51:32 -0800

    finish model and migrate url

--- url_shortner/URLShortener/app/models/shortened_url.rb
+++ url_shortner/URLShortener/app/models/shortened_url.rb
@@ -1,2 +1,9 @@
 class ShortenedUrl < ApplicationRecord
+    validate :user, :short_url, :long_url, presence: true
+    validate :short_url, uniqueness: true
+
+    belongs_to :user,
+        primary_key: :id,
+        foreign_key: :user,
+        class_name: :User
 end
--- url_shortner/URLShortener/db/schema.rb
+++ url_shortner/URLShortener/db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_08_215944) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_221324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shortened_urls", force: :cascade do |t|
+    t.string "long_url", null: false
+    t.string "short_url", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["short_url"], name: "index_shortened_urls_on_short_url", unique: true
+    t.index ["user_id"], name: "index_shortened_urls_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -21,4 +31,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_08_215944) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "shortened_urls", "users"
 end
